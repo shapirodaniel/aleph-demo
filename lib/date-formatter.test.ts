0f172418d410d497afeb6d0a8b266c7c94250054
Daniel Shapiro <daniel.shapiro@avanade.com>
2021-09-13 16:02:02 -0500

    add: tests

---
+++ lib/date-formatter.test.ts
@@ -0,0 +1,14 @@
+import { assertObjectMatch } from "https://deno.land/std@0.100.0/testing/asserts.ts";
+import getParsedDate from "./date-formatter.js";
+
+Deno.test("Date Formatter getParsedDate()", () => {
+  const dateObj = new Date("14 Jun 2020 15:00:00 PDT");
+  const parsedDateObject = getParsedDate(dateObj);
+  const expectedDateObject = {
+    weekday: "Sunday",
+    month: "June",
+    day: 14,
+    year: 2020,
+  };
+  assertObjectMatch(parsedDateObject, expectedDateObject);
+});
