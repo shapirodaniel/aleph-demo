9c4f481934c0ca153ae599192a2588352c9b297e
Daniel Shapiro <daniel.shapiro@avanade.com>
2021-09-13 12:21:40 -0500

    add: footer-links, footer logos

---
+++ footer-links.js
@@ -0,0 +1,17 @@
+export const footerLinks = [
+  {
+    name: "GET IN TOUCH",
+    links: ["Contact Us", "About Us", "Directions", "Blog"],
+  },
+  {
+    name: "OPPORTUNITIES",
+    links: ["Promotions", "Partners", "Careers", "FAQs"],
+  },
+];
+
+export const logoLinks = [
+  "/assets/logos/facebook.png",
+  "/assets/logos/instagram.png",
+  "/assets/logos/twitch.jpg",
+  "/assets/logos/twitter.png",
+];
