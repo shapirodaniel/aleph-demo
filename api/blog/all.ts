8fa286f84ad458096505b90525b8adf787fe59c4
Daniel Shapiro <daniel.shapiro@avanade.com>
2021-09-14 06:20:03 -0500

    chore: comment apiHandler data source

--- api/blog/all.ts
+++ api/blog/all.ts
@@ -1,4 +1,6 @@
 import type { APIHandler } from "aleph/types.d.ts";
+
+// in production, served from FaaS / db endpoint
 import blogEntries from "../../src/blog-entries.js";
 
 export const handler: APIHandler = ({ response }) => {
